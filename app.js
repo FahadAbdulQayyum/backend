6c709f3f9ba192610d433362a27dc522320d5b04
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-03-08 17:00:33 +0500

    added another api of /fahad

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Welcome to Fahad\'s World!');
 })
 
+app.get('/fahad', (req, res) => {
+    res.send('Welcome to \'fahad\'s page!');
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
